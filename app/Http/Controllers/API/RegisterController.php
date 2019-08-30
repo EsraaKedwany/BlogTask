7a0a933d08d1f518ccd560bc63cb5bc32e59490a
EsraaKedwany <esraa.mohamed1006@gmail.com>
2019-08-30 21:29:45 +0200

    Add register function to register controller

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -4,8 +4,34 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
+use JWTFactory;
+use JWTAuth;
+use Validator;
+use Response;
 
 class RegisterController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255|unique:users',
+            'name' => 'required',
+            'password'=> 'required',
+            'image'=> 'required|image',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password')),
+            'image' => $request->file('image')->store('avatars')
+        ]);
+        $user = User::first();
+        $token = JWTAuth::fromUser($user);
+        
+        return Response::json(compact('token'));
+    }
 }
