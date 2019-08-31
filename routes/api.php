2d949089ce826174c7d65c037008eab48e69e03f
EsraaKedwany <esraa.mohamed1006@gmail.com>
2019-08-31 23:41:26 +0200

    Add middleware

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::group(['middleware' => ['jwt.auth']], function () {
+
+// tweets routes
 Route::post('/tweets','API\TweetController@store');
 Route::delete('/tweets/{tweet}','API\TweetController@destroy');
 
-Route::post('/register', 'API\RegisterController@register');
-Route::post('/login', 'API\LoginController@login');
+//search/follow routes
 Route::get('/searchbyname/{name}', 'API\UserController@searchByName');
 Route::get('/searchbyemail/{email}', 'API\UserController@searchByEmail');
-
 Route::post('/follow/{user}', 'API\UserController@follow');
+
+});
+
+//user routes
+Route::post('/register', 'API\RegisterController@register');
+Route::post('/login', 'API\LoginController@login');
+
 
