61a46f8b728bda2aaf222cf05fb6b65c6ad0be68
EsraaKedwany <esraa.mohamed1006@gmail.com>
2019-09-02 03:41:33 +0200

    Add file to english folder

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'invalid_cred' => 'invalid_credentials',
+    'cnct' => 'could_not_create_token',
+
+    'no_res' => 'no result',
+    'wrong_id' => 'wrong id',
+    'follow' => 'follow successfully',
+
+];
